eba766dab14753813ef53b1d85ae6a8b6ce0d15d
jsuraj <jsurajbaramati@gmail.com>
2018-02-14 11:55:54 +0530

    removed gatsby-plugin-netlify-cms, trying manually

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,11 +35,11 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-netlify-cms',
+    //   options: {
+    //     modulePath: `${__dirname}/src/cms/cms.js`,
+    //   },
+    // },
   ],
 };
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
-import CMS from 'netlify-cms';
-import 'netlify-cms/dist/cms.css';
-
-import BlogPostPreview from './preview-templates/BlogPostPreview';
-
-CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
+// import CMS from 'netlify-cms';
+// import 'netlify-cms/dist/cms.css';
+//
+// import BlogPostPreview from './preview-templates/BlogPostPreview';
+//
+// CMS.registerPreviewStyle('/styles.css');
+// CMS.registerPreviewTemplate('blog', BlogPostPreview);
