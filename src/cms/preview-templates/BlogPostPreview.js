475416a9af7c285f3b5f397471c683ab3fbd020f
jsuraj <jsurajbaramati@gmail.com>
2018-02-18 00:38:42 +0530

    added netlify-cms from npm

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,12 +1,12 @@
-// import React from 'react';
-// import { BlogPostTemplate } from '../../templates/blog-post-template';
-//
-// const BlogPostPreview = ({ entry, widgetFor }) => (
-//   <BlogPostTemplate
-//     content={widgetFor('body')}
-//     description={entry.getIn(['data', 'description'])}
-//     title={entry.getIn(['data', 'title'])}
-//   />
-// );
-//
-// export default BlogPostPreview;
+import React from 'react';
+import { BlogPostTemplate } from '../../templates/blog-post-template';
+
+const BlogPostPreview = ({ entry, widgetFor }) => (
+  <BlogPostTemplate
+    content={widgetFor('body')}
+    description={entry.getIn(['data', 'description'])}
+    title={entry.getIn(['data', 'title'])}
+  />
+);
+
+export default BlogPostPreview;
