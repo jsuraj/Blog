e7e49ef23b9e586fed633cfcc3c7cdf97d63d015
jsuraj <jsurajbaramati@gmail.com>
2018-02-28 23:10:16 +0530

    changed siteurl, optimized typography, added robots.txt

--- siteconfig.js
+++ siteconfig.js
@@ -3,5 +3,5 @@ module.exports = {
   siteDescription: 'TechRathi is a leading blog on new technologies like Cryptocurrency, BlockChain etc.Get insights in the latest technology.',
   keywords: 'Cryptocurrency, Blockchain, Bitcoin, Ethereum, ICO, Tokens, Wallets',
   disqusShortName: 'jsuraj-disqus-com',
-  siteUrl: 'http://jsuraj.netlify.com'
+  siteUrl: 'http://www.techrathi.com'
 }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,8 @@
 import Typography from "typography";
 import lincolnTheme from 'typography-theme-lincoln'
 
+delete lincolnTheme.googleFonts;
+
 const typography = new Typography(lincolnTheme);
 
 export default typography;
